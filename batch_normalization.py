Fix batch normalization

batch_normalization.py
@@ -16,6 +16,7 @@ def batch_norm(inputs, is_training_phase):
     depth = inputs.get_shape()[-1].value
 
     batch_mean, batch_var = tf.nn.moments(inputs, [0], name = 'moments')
+    batch_std = tf.sqrt(batch_var)
     ema = tf.train.ExponentialMovingAverage(decay = 0.9)
     ema_apply_op = ema.apply([batch_mean, batch_var])
     ema_mean, ema_var = ema.average(batch_mean), ema.average(batch_var)
@@ -28,6 +29,6 @@ def batch_norm(inputs, is_training_phase):
       mean_var_with_update,
       lambda: (ema_mean, ema_var))
 
-    normed = (inputs - batch_mean) / batch_var
+    normed = (inputs - batch_mean) / batch_std
 
-    return normed
+    return normed, batch_mean, batch_std

ladder_network.py
@@ -133,7 +133,7 @@ class Model:
     with tf.name_scope("layer") as scope:
       weights = self._weight_variable([self._layer_size(inputs), output_size])
       pre_normalization = tf.matmul(inputs, weights)
-      pre_noise = batch_norm(pre_normalization, is_training_phase = is_training_phase)
+      pre_noise, batch_mean, batch_std = batch_norm(pre_normalization, is_training_phase = is_training_phase)
       pre_activation = pre_noise + tf.random_normal([output_size],
           mean = 0.0, stddev = noise_level)
       post_activation = non_linearity(self._beta_gamma(pre_activation))
@@ -142,9 +142,8 @@ class Model:
       layer_output.pre_normalization = pre_normalization
       layer_output.pre_activation = pre_activation
       layer_output.post_activation = post_activation
-      layer_output.batchmean = None
-      layer_output.batchstd = None
-
+      layer_output.batch_mean = batch_mean
+      layer_output.batch_std = batch_std
       return layer_output
 
   def _beta_gamma(self, inputs):