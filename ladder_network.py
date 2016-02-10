5eac5e4e3f2c3f270bbfa4ddbb410bfe6a0fffa4
Antti Tarvainen <antti.tarvainen@reaktor.com>
2016-02-10 06:22:50 +0200

    Extract EncoderLayer and DecoderLayer classes

--- ladder_network.py
+++ ladder_network.py
@@ -4,6 +4,49 @@ from tensorflow.python import control_flow_ops
 from batch_normalization import batch_norm
 from time import strftime
 
+
+
+class Session:
+  def __init__(self, model):
+    self.session = tf.Session()
+    self.model = model
+    self.writer = tf.train.SummaryWriter(
+        logdir = strftime("logs/%Y-%m-%d_%H:%M:%S"),
+        graph_def = self.session.graph_def)
+
+  def __enter__(self):
+    self.session.run(tf.initialize_all_variables())
+    return self
+
+  def __exit__(self, type, value, traceback):
+    self.session.close()
+
+  def train_supervised_batch(self, inputs, labels, step_number):
+    train_result, summary = self.session.run(
+      [self.model.supervised_train_step, self.model.supervised_summaries],
+      self.model.fill_placeholders(
+          inputs, labels, is_training_phase = True))
+
+    self.writer.add_summary(summary, step_number)
+    return train_result
+
+  def train_unsupervised_batch(self, inputs, step_number):
+    train_result, summary = self.session.run(
+        [self.model.unsupervised_train_step, self.model.unsupervised_summaries],
+        self.model.fill_placeholders(inputs, is_training_phase = True))
+
+    self.writer.add_summary(summary, step_number)
+    return train_result
+
+  def test(self, inputs, labels, step_number):
+    accuracy, summary = self.session.run(
+        [self.model.accuracy_measure, self.model.test_summaries],
+        self.model.fill_placeholders(inputs, labels, is_training_phase = False))
+
+    self.writer.add_summary(summary, step_number)
+    return accuracy
+
+
 class Model:
   def __init__(self, input_layer_size, class_count):
     self.hyperparameters = {
@@ -28,7 +71,7 @@ class Model:
 
   def fill_placeholders(self, inputs, labels = None, is_training_phase = True):
     if labels is None:
-      labels = numpy.zeros([inputs.shape[0], self._layer_size(self.placeholders.labels)])
+      labels = numpy.zeros([inputs.shape[0], _layer_size(self.placeholders.labels)])
     return {
       self.placeholders.inputs: inputs,
       self.placeholders.labels: labels,
@@ -50,7 +93,7 @@ class Model:
     encoder_layer_definitions = [
       (100, tf.nn.relu),
       (50, tf.nn.relu),
-      (self._layer_size(placeholders.labels), tf.nn.softmax)
+      (_layer_size(placeholders.labels), tf.nn.softmax)
     ]
     clean_encoder_outputs = self._encoder_layers(
         input_layer = placeholders.inputs,
@@ -100,7 +143,7 @@ class Model:
       input_layer, other_layer_definitions,
       noise_level, is_training_phase):
     with tf.name_scope("encoder") as scope:
-      input_layer_size = self._layer_size(input_layer)
+      input_layer_size = _layer_size(input_layer)
 
       first_encoder_layer = self._Record()
       first_encoder_layer.post_activation = input_layer
@@ -110,7 +153,7 @@ class Model:
 
       layer_accumulator = [first_encoder_layer]
       for (layer_size, non_linearity) in other_layer_definitions:
-        layer_output = self._fully_connected_layer(
+        layer_output = _EncoderLayer(
             inputs = layer_accumulator[-1].post_activation,
             output_size = layer_size,
             non_linearity = non_linearity,
@@ -127,7 +170,7 @@ class Model:
       last_clean_encoder_layer = clean_encoder_layers[-1]
       rest_of_encoder_layers = zip(clean_encoder_layers, corrupted_encoder_layers)[:-1]
 
-      first_decoder_layer = self._decoder_layer(
+      first_decoder_layer = _DecoderLayer(
           previous_decoder_layer = last_corrupted_encoder_layer,
           corrupted_encoder_layer = last_corrupted_encoder_layer,
           clean_encoder_layer = last_clean_encoder_layer,
@@ -137,67 +180,13 @@ class Model:
 
       layer_accumulator = [first_decoder_layer]
       for clean_layer, corrupted_layer in reversed(rest_of_encoder_layers):
-        layer = self._decoder_layer(
+        layer = _DecoderLayer(
             previous_decoder_layer = layer_accumulator[-1],
             clean_encoder_layer = clean_layer,
             corrupted_encoder_layer = corrupted_layer,
             is_training_phase = is_training_phase)
         layer_accumulator.append(layer)
       return layer_accumulator
-
-  def _fully_connected_layer(self,
-      inputs, output_size, non_linearity,
-      noise_level, is_training_phase):
-    with tf.name_scope("layer") as scope:
-      weights = self._weight_variable([self._layer_size(inputs), output_size])
-      pre_normalization = tf.matmul(inputs, weights)
-      pre_noise, batch_mean, batch_std = batch_norm(pre_normalization, is_training_phase = is_training_phase)
-      pre_activation = pre_noise + tf.random_normal([output_size],
-          mean = 0.0, stddev = noise_level)
-      post_activation = non_linearity(self._beta_gamma(pre_activation))
-
-      layer_output = self._Record()
-      layer_output.pre_normalization = pre_normalization
-      layer_output.pre_activation = pre_activation
-      layer_output.post_activation = post_activation
-      layer_output.batch_mean = batch_mean
-      layer_output.batch_std = batch_std
-      return layer_output
-
-  def _decoder_layer(self,
-      previous_decoder_layer, clean_encoder_layer, corrupted_encoder_layer,
-      is_training_phase, is_first_decoder = False):
-    with tf.name_scope("decoder_layer") as scope:
-      input_size = self._layer_size(previous_decoder_layer.post_activation)
-      output_size = self._layer_size(clean_encoder_layer.post_activation)
-
-      if is_first_decoder:
-        pre_1st_normalization = previous_decoder_layer.post_activation
-      else:
-        weights = self._weight_variable([input_size, output_size])
-        pre_1st_normalization = tf.matmul(
-          previous_decoder_layer.post_activation, weights)
-
-      pre_activation, _, _ = batch_norm(pre_1st_normalization, is_training_phase = is_training_phase)
-      post_activation = tf.nn.relu(pre_activation)
-
-      post_2nd_normalization = \
-        (post_activation - clean_encoder_layer.batch_mean) / clean_encoder_layer.batch_std
-
-      layer_output = self._Record()
-      layer_output.pre_activation = pre_activation
-      layer_output.post_activation = post_activation
-      layer_output.post_2nd_normalization = post_2nd_normalization
-      return layer_output    
-
-  def _beta_gamma(self, inputs):
-    layer_size = self._layer_size(inputs);
-    beta = tf.Variable(tf.constant(0.0,
-        shape = [layer_size]), name = 'beta', trainable = True)
-    gamma = tf.Variable(tf.constant(1.0,
-        shape = [layer_size]), name = 'gamma', trainable = True)
-    return gamma * (inputs + beta)
-
 
   def _optimizer(self, learning_rate, cost_function):
     with tf.name_scope("optimizer") as scope:
@@ -237,56 +226,60 @@ class Model:
       tf.scalar_summary("cross entropy", cross_entropy, ["supervised"])
       return cross_entropy
 
-  def _weight_variable(self, shape):
-    with tf.name_scope("weight") as scope:
-      initial = tf.truncated_normal(shape, stddev = 0.1)
-      return tf.Variable(initial)
+def _weight_variable(shape):
+  with tf.name_scope("weight") as scope:
+    initial = tf.truncated_normal(shape, stddev = 0.1)
+    return tf.Variable(initial)
 
-  def _bias_variable(self, shape):
-    with tf.name_scope("bias") as scope:
-      initial = tf.constant(0.1, shape = shape)
-      return tf.Variable(initial)
-
-  def _layer_size(self, layer_output):
-    return layer_output.get_shape()[-1].value
+def _layer_size(layer_output):
+  return layer_output.get_shape()[-1].value
 
 
-class Session:
-  def __init__(self, model):
-    self.session = tf.Session()
-    self.model = model
-    self.writer = tf.train.SummaryWriter(
-        logdir = strftime("logs/%Y-%m-%d_%H:%M:%S"),
-        graph_def = self.session.graph_def)
 
-  def __enter__(self):
-    self.session.run(tf.initialize_all_variables())
-    return self
+class _EncoderLayer:
+  def __init__(self,
+      inputs, output_size, non_linearity,
+      noise_level, is_training_phase):
+    with tf.name_scope("encoder_layer") as scope:
+      weights = _weight_variable([_layer_size(inputs), output_size])
+      self.pre_normalization = tf.matmul(inputs, weights)
+      pre_noise, self.batch_mean, self.batch_std = batch_norm(
+          self.pre_normalization, is_training_phase = is_training_phase)
+      self.pre_activation = pre_noise + tf.random_normal(
+          [output_size], mean = 0.0, stddev = noise_level)
+      self.post_activation = non_linearity(self._beta_gamma(self.pre_activation))
 
-  def __exit__(self, type, value, traceback):
-    self.session.close()
+  def _beta_gamma(self, inputs):
+    layer_size = _layer_size(inputs);
+    beta = tf.Variable(tf.constant(0.0,
+        shape = [layer_size]), name = 'beta', trainable = True)
+    gamma = tf.Variable(tf.constant(1.0,
+        shape = [layer_size]), name = 'gamma', trainable = True)
+    return gamma * (inputs + beta)
 
-  def train_supervised_batch(self, inputs, labels, step_number):
-    train_result, summary = self.session.run(
-      [self.model.supervised_train_step, self.model.supervised_summaries],
-      self.model.fill_placeholders(
-          inputs, labels, is_training_phase = True))
 
-    self.writer.add_summary(summary, step_number)
-    return train_result
+class _DecoderLayer:
+  def __init__(self,
+      previous_decoder_layer, clean_encoder_layer, corrupted_encoder_layer,
+      is_training_phase, is_first_decoder = False):
+    with tf.name_scope("decoder_layer") as scope:
+      input_size = _layer_size(previous_decoder_layer.post_activation)
+      output_size = _layer_size(clean_encoder_layer.post_activation)
 
-  def train_unsupervised_batch(self, inputs, step_number):
-    train_result, summary = self.session.run(
-        [self.model.unsupervised_train_step, self.model.unsupervised_summaries],
-        self.model.fill_placeholders(inputs, is_training_phase = True))
+      if is_first_decoder:
+        pre_1st_normalization = previous_decoder_layer.post_activation
+      else:
+        weights = _weight_variable([input_size, output_size])
+        pre_1st_normalization = tf.matmul(
+          previous_decoder_layer.post_activation, weights)
 
-    self.writer.add_summary(summary, step_number)
-    return train_result
+      pre_activation, _, _ = batch_norm(pre_1st_normalization, is_training_phase = is_training_phase)
+      post_activation = tf.nn.relu(pre_activation)
 
-  def test(self, inputs, labels, step_number):
-    accuracy, summary = self.session.run(
-        [self.model.accuracy_measure, self.model.test_summaries],
-        self.model.fill_placeholders(inputs, labels, is_training_phase = False))
+      post_2nd_normalization = \
+        (post_activation - clean_encoder_layer.batch_mean) / clean_encoder_layer.batch_std
 
-    self.writer.add_summary(summary, step_number)
-    return accuracy
+      self.pre_activation = pre_activation
+      self.post_activation = post_activation
+      self.post_2nd_normalization = post_2nd_normalization
+
